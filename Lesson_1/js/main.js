93af5286d55f3b82dcdaf14b5645fe525ddd9ead
Mike <michael.lenard.jackson@gmail.com>
2020-04-25 18:57:30 -0500

    Introduction to while loops

--- Lesson_1/js/main.js
+++ Lesson_1/js/main.js
@@ -1,8 +1,15 @@
 var name = "Mike";
 var val = 6;
 
-var myList = [ 1, 2, 3, 4, 5];
+var myList = [ 1, 2, 3, 4, 5, 6, 7];
 
-for(var i = 0; i < myList.length; i++){
+for(var i = 0; i < myList[i]; i++){
 	console.log(myList[i]);
+}
+
+
+var i = 0;
+while( myList[i] ){
+	console.log(myList[i]);
+	i++;
 }
