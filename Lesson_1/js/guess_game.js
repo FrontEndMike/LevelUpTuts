838b39349ad9299eab9c06b024ea4df5fd921bf7
Mike <michael.lenard.jackson@gmail.com>
2020-04-25 20:04:30 -0500

    Create guessing game
    Create guessing game that counts guess, verify guess is between range, and notifies when thier is 1 guess left and last guess.

--- Lesson_1/js/guess_game.js
+++ Lesson_1/js/guess_game.js
@@ -5,12 +5,16 @@ var guess = prompt("Whats your guess?");
 var guesses;
 
 for(i = 0; i < 5; i++){
+	//Count number of gues
 	var j = i + 1;
 	guesses = 5 - j;
+	//Verify guess is between 1 & 10
 	if( guess > 10 || guess < 1){
+		//Notify on second to last guess
 		if( guesses == 1 ){
 			guess = prompt("Please guess a number between 1 and 10. You have " + guesses + " guess left.");
 		}
+		//Notify on last guess
 		else if( guesses == 0 ){
 			guess = prompt("Please guess a number between 1 and 10. You are on your last guess.");
 		}
@@ -22,6 +26,7 @@ for(i = 0; i < 5; i++){
 		alert("You guessed correctly");
 		break;
 	}
+	//If guess is between 1 & 10 but still not correct.
 	else{
 		if( guesses == 1 ){
 			guess = prompt("Please Try Again. You have " + guesses + " guess left.");
