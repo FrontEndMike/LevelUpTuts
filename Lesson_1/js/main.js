6bd42f61efa97e76b62a4d1345d150778d774821
Mike <michael.lenard.jackson@gmail.com>
2020-04-26 16:33:20 -0500

    Introduction to functions

--- Lesson_1/js/main.js
+++ Lesson_1/js/main.js
@@ -1,36 +1,13 @@
-alert("You have 5 guesses. Guess 1 - 10");
+var name = "Mike";
 
-var answer = 3;
-var guess = prompt("Whats your guess?");
-var guesses;
-
-for(i = 0; i < 5; i++){
-	var j = i + 1;
-	guesses = 5 - j;
-	if( guess > 10 || guess < 1){
-		if( guesses == 1 ){
-			guess = prompt("Please guess a number between 1 and 10. You have " + guesses + " guess left.");
-		}
-		else if( guesses == 0 ){
-			guess = prompt("Please guess a number between 1 and 10. You are on your last guess.");
-		}
-		else{
-			guess = prompt("Please guess a number between 1 and 10. You have " + guesses + " guesses left.");
-		}
-	}
-	else if( answer == guess ){
-		alert("You guessed correctly");
-		break;
-	}
-	else{
-		if( guesses == 1 ){
-			guess = prompt("Please Try Again. You have " + guesses + " guess left.");
-		}
-		else if( guesses == 0 ){
-			guess = prompt("Please Try Again. You are on your last guess.");
-		}
-		else{
-			guess = prompt("Please Try Again. You have " + guesses + " guesses left.");
-		}
-	}
+function firstFunc(){
+	console.log("yo yo");
+	//Private variable
+	var name = "Bill";
+	console.log("Name in function " + name);
 }
+
+//Runs firstFunc function
+firstFunc();
+
+console.log(name);
