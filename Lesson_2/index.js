fb153e75bac3426c35ffa16133e7ab147403950f
Mike Jackson <michael.lenard.jackson@gmail.com>
2020-05-01 18:18:20 -0500

    Interacting With The DOM

--- Lesson_2/index.js
+++ Lesson_2/index.js
@@ -1,11 +1,6 @@
-document.getElementById("app").innerHTML = `
-<h1>Hello Parcel!</h1>
-<div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
-</div>
-`;
+const logo = document.querySelector("#logo");
+
+logo.innerText = "yo!";
 
 const heading = document.createElement('div')
 heading.innerHTML = `
