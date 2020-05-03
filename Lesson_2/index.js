bf89351473adeb6656cda7d788c6a2dd2789058b
Mike <michael.lenard.jackson@gmail.com>
2020-05-03 13:37:16 -0500

    Introduction to Promises

--- Lesson_2/index.js
+++ Lesson_2/index.js
@@ -1,38 +1,19 @@
-// Generate random color
-// //  RBG 0-255
-// Apply to dom
-// update color on event
+// Made up code
 
-const generateColorValue = () => Math.floor(Math.random() * 256);
-const createColor = () => {
-	const red = generateColorValue();
-	const green = generateColorValue();
-	const blue = generateColorValue();
-	return `rgb(${red}, ${green}, ${blue})`
-}
+const chargeCard = () => new Promise(( resolve, reject ) => {
+	// card is valid
+	reject("Fail!!!");
+	if (true) {
+		return resolve(true);
+	}
+	return resolve(false);
+});
 
-const applyColorToBody = color => {
-	document.body.style.backgroundColor = color;
-}
 
-const addRandomtoBackground = () => {
-	const color = createColor();
-	return applyColorToBody(color);
-}
-
-addRandomtoBackground();
-
-const newColors = document.getElementById("new-colors");
-newColors.addEventListener("click", addRandomtoBackground)
-
-// Set Interval vs Set Timeout
-
-// const log = () => console.log("is in timeout");
-// setTimeout(log, 1000);
-// setTimeout(addRandomtoBackground, 5000);
-// Set Timeout happens once
-
-const interval = setInterval(addRandomtoBackground, 2000);
-//Set Interval, happens over and over
-
-newColors.addEventListener("click", () => clearInterval(interval));
+chargeCard()
+.then((val) => {
+	console.log(val);
+})
+.catch((err) => {
+	console.log(err);
+});
