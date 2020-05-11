b2db13baab180d97903c0ee81a6c416c0197e00f
Mike Jackson <michael.lenard.jackson@gmail.com>
2020-05-11 17:21:39 -0500

    Refresher on .map array function

--- Lesson_2/index.js
+++ Lesson_2/index.js
@@ -17,3 +17,23 @@ const shoppingCart = [
     type: "tutorial"
   }
 ];
+
+
+// .map
+// iterates over array, perform funtion on each item
+// returns new array
+// Use when you build an array out of old data and need to add
+// new data to it or modify data in array
+
+// const discountCart = simpleShoppingCart.map( value =>  value * .75);
+
+const discountCart = shoppingCart.map((total) => {
+  return{
+    ...total,
+    salePrice: total.price * .75
+  }
+});
+
+
+console.log(shoppingCart);
+console.log(discountCart);
