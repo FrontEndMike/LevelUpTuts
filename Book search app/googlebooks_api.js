d32cac39c9cba7547243ab2da9e20f4eff826076
Mike <michael.lenard.jackson@gmail.com>
2020-05-19 00:15:12 -0500

    Add functionality of collapsable description

--- Book search app/googlebooks_api.js
+++ Book search app/googlebooks_api.js
@@ -4,7 +4,7 @@
 
 
 const SEARCH = document.getElementById("search");
-
+let counter = 0;
 function createNode(element) {
 	return document.createElement(element);
 }
@@ -40,14 +40,39 @@ function searchFunc() {
 			let author_name = new_list.volumeInfo.authors;
             let book_title = new_list.volumeInfo.title;
             let book_link = new_list.volumeInfo.canonicalVolumeLink;
-            let book_img = new_list.volumeInfo.imageLinks.thumbnail;
-			li = createNode('li'),
-            li.innerHTML = `<a href="${book_link}" target="_blank">
-                            <img src="${book_img}">
-                            <span class="title">${book_title} </span>
-                            <span class="aurthor"> Author : ${author_name} </span>
-                            </a>`;
-            append(ul, li);
+			let book_img = new_list.volumeInfo.imageLinks.thumbnail;
+			let book_desc = new_list.volumeInfo.description;
+			li = createNode('li')
+			li.setAttribute("class", "list-group-item");
+			li.innerHTML = `<div class="book-image">
+								<a href="${book_link}" target="_blank">
+								<img src="${book_img}"></a>
+							</div>
+							<div class="book-info">
+                            	<h3 class="title">${book_title} </h3>
+								<p class="aurthor"> Author : ${author_name} </p>
+								<a href="${book_link}" target="_blank" class="btn btn-primary">
+									Purchase
+								</a>
+								<p class="description-btn">
+								<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#multiCollapseExample${counter}" aria-expanded="false" aria-controls="multiCollapseExample${counter}">
+									Description
+								</button>
+								</p>
+								<div class="row">
+								<div class="col">
+									<div class="collapse multi-collapse" id="multiCollapseExample${counter}">
+									<div class="card card-body">
+										${book_desc}
+									</div>
+									</div>
+								</div>
+								</div>
+							</div>
+							`;
+			counter++;
+			append(ul, li);
+			console.log(book_list);
         }) 
 	})
 	.catch(error => {
